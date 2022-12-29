94a1e784f59532803ad6d0384a4e026df822fd9c
Chirayu Desai <chirayudesai1@gmail.com>
2022-12-30 00:13:29 +0530

    Add a stub camera2 app with the icon
    TODOS:
    1. Good way to avoid duplicate icon
       Easiest is to remove icon from Aperture
       Harder is some form of BootReceiver to disable, maybe
       if first boot of OS, disable camera2, else disable aperture
    2. Any other issues?
    
    Change-Id: Ief26f15670081b0e304736b19f52e471b5763146

---
+++ icon/src/com/android/camera/CameraLauncher.kt
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.camera
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import com.android.camera.utils.ShortcutsUtils
+
+open class CameraLauncher : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Register shortcuts
+        ShortcutsUtils.registerShortcuts(this)
+
+        val apertureIntent = Intent()
+            .setComponent(ComponentName("org.lineageos.aperture", "org.lineageos.aperture.CameraActivity"))
+            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(apertureIntent)
+        finish()
+    }
+}
