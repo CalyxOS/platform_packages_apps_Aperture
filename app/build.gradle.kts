b3fa5ab9803dc4f228876e22502a64624262d3fd
Chirayu Desai <chirayudesai1@gmail.com>
2022-12-30 00:39:12 +0530

    Use aapt to rename to com.android.camera2
    * Rename launcher activity too
    * Just to keep launcher icon on upgrades
    
    Change-Id: Ib6cbacf797f508828308bb781dc47a1a59b2561f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     namespace = "org.lineageos.aperture"
 
     defaultConfig {
-        applicationId = "org.lineageos.aperture"
+        applicationId = "com.android.camera2"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
